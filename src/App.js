c86114011ee3d952d83f25cec0efde252ede6c19
junzaivip <goxuexi@qq.com>
2019-09-02 17:22:59 +0800

    常规写法，更新val的值

--- src/App.js
+++ src/App.js
@@ -15,17 +15,53 @@ import Footer from './components/Footer'
 import Jumbotron from './components/Jumbotron'
 
 
+const Temp =(props)=>{
+    console.log("render Temp")
+    return (
+        <div>
+            {props.val}
+        </div>
+    )
+}
+
+
 class App extends Component {
+
+    state = {
+        val : 1
+    }
+
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({val:1})
+
+        },2000)
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log("nextState: ", nextState);
+        console.log("Current State:", this.state);
+
+     return  (
+         this.state.val === nextState.val ? false : true
+     )
+
+    }
+
+
   render() {
+        console.log("render app")
     return (
         <Router>
             <div>
                 <Navbar />
-                <Jumbotron  title="About Me!" subtitle="This page is all about me and my work!"/>
+                <Temp val = {this.state.val}></Temp>
+                <Jumbotron  title="About Me!" subtitle={this.state.val}/>
                 <Route path="/" exact component={Home}></Route>
                 <Route path="/about/:id" component={About}></Route>
                 <Route path="/contact" component={Contact}></Route>
                 <Footer />
+
             </div>
         </Router>
 
