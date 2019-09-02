cecaafe2db55533da2906587a3a2b342a16e2f6b
junzaivip <goxuexi@qq.com>
2019-09-02 17:32:24 +0800

    子页面不刷新方法

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component, PureComponent} from 'react';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About.js';
 import Contact from './pages/Contact.js';
 
 
-
-
-import { HashRouter as Router, Route } from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 
 
 import Navbar from './components/Navbar'
@@ -15,58 +13,60 @@ import Footer from './components/Footer'
 import Jumbotron from './components/Jumbotron'
 
 
-const Temp =(props)=>{
-    console.log("render Temp")
-    return (
-        <div>
-            {props.val}
-        </div>
-    )
+class Temp extends PureComponent {
+    render(){
+        return(
+            <div>
+                {this.props.val}
+            </div>
+        )
+    }
+
 }
 
 
 class App extends Component {
 
     state = {
-        val : 1
+        val: 1
     }
 
-    componentDidMount(){
-        setInterval(()=>{
-            this.setState({val:1})
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({val: 1})
 
-        },2000)
+        }, 2000)
     }
 
-    shouldComponentUpdate(nextProps, nextState){
-        console.log("nextState: ", nextState);
-        console.log("Current State:", this.state);
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.log("nextState: ", nextState);
+    //     console.log("Current State:", this.state);
+    //
+    //  return  (
+    //      this.state.val === nextState.val ? false : true
+    //  )
+    //
+    // }
 
-     return  (
-         this.state.val === nextState.val ? false : true
-     )
 
-    }
-
-
-  render() {
+    render() {
         console.log("render app")
-    return (
-        <Router>
-            <div>
-                <Navbar />
-                <Temp val = {this.state.val}></Temp>
-                <Jumbotron  title="About Me!" subtitle={this.state.val}/>
-                <Route path="/" exact component={Home}></Route>
-                <Route path="/about/:id" component={About}></Route>
-                <Route path="/contact" component={Contact}></Route>
-                <Footer />
+        return (
+            <Router>
+                <div>
+                    <Navbar/>
+                    <Temp val={this.state.val}></Temp>
+                    <Jumbotron title="About Me!" subtitle={this.state.val}/>
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/about/:id" component={About}></Route>
+                    <Route path="/contact" component={Contact}></Route>
+                    <Footer/>
 
-            </div>
-        </Router>
+                </div>
+            </Router>
 
-    );
-  }
+        );
+    }
 }
 
 export default App;
